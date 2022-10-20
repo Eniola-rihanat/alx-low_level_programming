c651fc6a46377ade0f0540f0e4e60692abdc9855
Eniola-rihanat <rihanatlawal@gmail.com>
2022-10-20 16:12:41 -0700

    task list

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,16 @@
+#ifndef LISTS_H
+#define LIST_H
+#include <stdio.h>
+#include <stdib.h>
+#include <string.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - ml'ti
+ * @len: length of the string
+ * x:it  he ne
+ * collot s  nkedro
+ * r onee n satofthsfo suel
+ * prgmcoltn
+ */
+:wq
+:w:
