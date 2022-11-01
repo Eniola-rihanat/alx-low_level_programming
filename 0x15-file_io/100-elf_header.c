0336f7935c6f10eb4fa533fe20e1c0fa02c711f6
Eniola-rihanat <110996115+Eniola-rihanat@users.noreply.github.com>
2022-11-01 02:31:14 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
-#include <elf.h>
+#include "elf.h"
+
+/*void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);*/
 
 /**
- * get_type - determine file type
- * @ehdr: pointer to struct of ELF header features
- */
-void get_type(Elf64_Ehdr *ehdr)
+  *check_elf - checks if a file is an ELF file.
+  *@e_ident: a pointer to an array containing the ELF magis number
+  *Description: if the file is not ELF file exit 98
+  */
+/*void check_elf(unsigned char *e_ident)
 {
-	printf("%-35s", "Type:");
-	switch (ehdr->e_type)
+	int index;
+	for (index = 0; index < 4; index++)
 	{
-	case ET_NONE:
-		printf("NONE (Unknown type)\n");
-		break;
-	case ET_REL:
-		printf("REL (Relocatable file)\n");
-		break;
-	case ET_EXEC:
-		printf("EXEC (Executable file)\n");
-		break;
-	case ET_DYN:
-		printf("DYN (Shared object file)\n");
-		break;
-	case ET_CORE:
-		printf("CORE (Core file)\n");
-		break;
+		if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: not an ELF file\n");
+			exit(98);
+		}
 	}
-}
-
-
+}*/
 
 /**
- * main - entry point for program to get header of ELF file
- * @argc: arg of count
- * @argv: arg of array
- *
+ * main - The entry point for program to get header of ELF file
+ * @argc: The number of arguments
+ * @argv: The pointer to array of arguments
  * Return: 1 on success, error code on failure
  */
 int main(int argc, char *argv[])
