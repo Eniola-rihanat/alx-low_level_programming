afe0d5183125092dced09460e0f685450de19b17
Eniola-rihanat <110996115+Eniola-rihanat@users.noreply.github.com>
2022-12-14 06:09:05 +0100

    Create 3-islower.c

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character
+ *
+ * @c: checks input of function
+ *
+ * Return: 1 if c is lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
